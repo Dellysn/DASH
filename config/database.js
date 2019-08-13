fb67832e8b83adcb1ab59a22c8e77783750d2bbd
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2019-08-13 17:04:23 +0100

    mlab added

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MongoURI: "mongodb://localhost:27017/ideabook"
+  // MongoURI: "mongodb://localhost:27017/ideabook"
+  mongodb: "//dellyson:dellyson1@ds263107.mlab.com:63107/dash"
 };
